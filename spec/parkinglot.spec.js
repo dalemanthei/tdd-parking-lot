faac746a9b1d0730e09ffbae0f3ff8a2e0a41646
Dale Manthei <dalemanthei@users.noreply.github.com>
2017-07-23 17:14:31 -0500

    fix(project): refine the testing based on feedback

--- spec/parkinglot.spec.js
+++ spec/parkinglot.spec.js
@@ -10,97 +10,113 @@ describe('ParkingLot', () => {
     expect(parkingLot.vacancies()).toEqual(5)
   })
 
-  //   it('starts off with vacancies', () => {
-  //     let parkingLot = new ParkingLot(7)
-  //     expect(parkingLot.vacancies()).toEqual(7)
-  //   })
+  // it('starts off with vacancies', () => {
+  //   let parkingLot = new ParkingLot(7)
+  //   expect(parkingLot.vacancies()).toEqual(7)
+  // })
 
   // it('starts off with vacancies', () => {
   //   let parkingLot = new ParkingLot(12)
   //   expect(parkingLot.vacancies()).toEqual(12)
   // })
 
-  // it('defaults to 5 vacancies', () => {
-  //   let parkingLot = new ParkingLot()
-  //   expect(parkingLot.vacancies()).toEqual(5)
+  // it('can print a summary of the parking lot', () => {
+  //   let parkingLot = new ParkingLot(4)
+  //   expect(parkingLot.summary()).toEqual([
+  //     'Spot 1: (empty)',
+  //     'Spot 2: (empty)',
+  //     'Spot 3: (empty)',
+  //     'Spot 4: (empty)'
+  //   ])
   // })
 
   // it('can print a summary of the parking lot', () => {
   //   let parkingLot = new ParkingLot(4)
   //   expect(parkingLot.summary()).toEqual([
-  //     'Position 1: (empty)',
-  //     'Position 2: (empty)',
-  //     'Position 3: (empty)',
-  //     'Position 4: (empty)'
+  //     'Spot 1: (empty)',
+  //     'Spot 2: (empty)',
+  //     'Spot 3: (empty)',
+  //     'Spot 4: (empty)'
   //   ])
   //   parkingLot = new ParkingLot(3)
   //   expect(parkingLot.summary()).toEqual([
-  //     'Position 1: (empty)',
-  //     'Position 2: (empty)',
-  //     'Position 3: (empty)'
+  //     'Spot 1: (empty)',
+  //     'Spot 2: (empty)',
+  //     'Spot 3: (empty)'
   //   ])
+  // })
+
+  // it('should decrement vacancies when a car is parked', () => {
+  //   let parkingLot = new ParkingLot(3)
+  //   expect(parkingLot.vacancies()).toEqual(3)
+  //   parkingLot.park('Car1')
+  //   expect(parkingLot.vacancies()).toEqual(2)
   // })
 
   // it('records that a car with the given license plate was parked and decreases vacancies', () => {
   //   let parkingLot = new ParkingLot(3)
 
-  //   parkingLot.park('aaa-123')
+  //   parkingLot.park('Car-1')
   //   expect(parkingLot.vacancies()).toEqual(2)
   //   expect(parkingLot.summary()).toEqual([
-  //     'Position 1: aaa-123',
-  //     'Position 2: (empty)',
-  //     'Position 3: (empty)'
+  //     'Spot 1: Car-1',
+  //     'Spot 2: (empty)',
+  //     'Spot 3: (empty)'
   //   ])
+  // })
 
-  //   parkingLot.park('bbb-456')
+  // it('records that a car with the given license plate was parked and decreases vacancies', () => {
+  //   let parkingLot = new ParkingLot(3)
+  //   parkingLot.park('Car-1')
+  //   parkingLot.park('Car-2')
   //   expect(parkingLot.vacancies()).toEqual(1)
   //   expect(parkingLot.summary()).toEqual([
-  //     'Position 1: aaa-123',
-  //     'Position 2: bbb-456',
-  //     'Position 3: (empty)'
+  //     'Spot 1: Car-1',
+  //     'Spot 2: Car-2',
+  //     'Spot 3: (empty)'
   //   ])
   // })
 
   // it('fills in empty spaces first', () => {
   //   let parkingLot = new ParkingLot(5)
-  //   parkingLot.park('aaa-123')
-  //   parkingLot.park('bbb-456') // this one will leave
+  //   parkingLot.park('Car-1')
+  //   parkingLot.park('Car-2') // this one will leave
   //   parkingLot.park('ccc-789')
 
-  //   parkingLot.leave('bbb-456')
+  //   parkingLot.leave('Car-2')
 
   //   expect(parkingLot.vacancies()).toEqual(3)
   //   expect(parkingLot.summary()).toEqual([
-  //     'Position 1: aaa-123',
-  //     'Position 2: (empty)',
-  //     'Position 3: ccc-789',
-  //     'Position 4: (empty)',
-  //     'Position 5: (empty)'
+  //     'Spot 1: Car-1',
+  //     'Spot 2: (empty)',
+  //     'Spot 3: ccc-789',
+  //     'Spot 4: (empty)',
+  //     'Spot 5: (empty)'
   //   ])
 
   //   parkingLot.park('ddd-321')
   //   expect(parkingLot.summary()).toEqual([
-  //     'Position 1: aaa-123',
-  //     'Position 2: ddd-321',
-  //     'Position 3: ccc-789',
-  //     'Position 4: (empty)',
-  //     'Position 5: (empty)'
+  //     'Spot 1: Car-1',
+  //     'Spot 2: ddd-321',
+  //     'Spot 3: ccc-789',
+  //     'Spot 4: (empty)',
+  //     'Spot 5: (empty)'
   //   ])
 
   //   parkingLot.park('eee-555')
   //   expect(parkingLot.summary()).toEqual([
-  //     'Position 1: aaa-123',
-  //     'Position 2: ddd-321',
-  //     'Position 3: ccc-789',
-  //     'Position 4: eee-555',
-  //     'Position 5: (empty)'
+  //     'Spot 1: Car-1',
+  //     'Spot 2: ddd-321',
+  //     'Spot 3: ccc-789',
+  //     'Spot 4: eee-555',
+  //     'Spot 5: (empty)'
   //   ])
   // })
 
   // it('can compact cars in the lot', () => {
   //   let parkingLot = new ParkingLot(8)
 
-  //   parkingLot.park('aaa-123')
+  //   parkingLot.park('Car-1')
   //   parkingLot.park('bbb-123')
   //   parkingLot.park('ccc-123')
   //   parkingLot.park('ddd-123')
@@ -115,27 +131,27 @@ describe('ParkingLot', () => {
   //   parkingLot.leave('ggg-123')
 
   //   expect(parkingLot.summary()).toEqual([
-  //     'Position 1: aaa-123',
-  //     'Position 2: (empty)',
-  //     'Position 3: ccc-123',
-  //     'Position 4: (empty)',
-  //     'Position 5: eee-123',
-  //     'Position 6: (empty)',
-  //     'Position 7: (empty)',
-  //     'Position 8: hhh-123'
+  //     'Spot 1: Car-1',
+  //     'Spot 2: (empty)',
+  //     'Spot 3: ccc-123',
+  //     'Spot 4: (empty)',
+  //     'Spot 5: eee-123',
+  //     'Spot 6: (empty)',
+  //     'Spot 7: (empty)',
+  //     'Spot 8: hhh-123'
   //   ])
 
   //   parkingLot.compact()
 
   //   expect(parkingLot.summary()).toEqual([
-  //     'Position 1: aaa-123',
-  //     'Position 2: hhh-123',
-  //     'Position 3: ccc-123',
-  //     'Position 4: eee-123',
-  //     'Position 5: (empty)',
-  //     'Position 6: (empty)',
-  //     'Position 7: (empty)',
-  //     'Position 8: (empty)'
+  //     'Spot 1: Car-1',
+  //     'Spot 2: hhh-123',
+  //     'Spot 3: ccc-123',
+  //     'Spot 4: eee-123',
+  //     'Spot 5: (empty)',
+  //     'Spot 6: (empty)',
+  //     'Spot 7: (empty)',
+  //     'Spot 8: (empty)'
   //   ])
   // })
 })
